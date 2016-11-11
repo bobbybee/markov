8d96070025899207b7d51dc88ffbef153755684e
Alyssa Rosenzweig <alyssa@rosenzweig.io>
2016-11-11 12:55:14 -0800

    Initial commit

---
+++ markov.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <map>
+
+typedef std::map<std::string, std::map<std::string, std::map<std::string, int>>> MarkovChain;
+
+int main() {
+    printf("Hello World!\n");
+}

a173ff0a142323342db82f5fab935ca62260829e
nickolas360 <nickolas360@users.noreply.github.com>
2016-11-12 06:04:10 +0000

    Minor formatting changes

--- markov.cpp
+++ markov.cpp
@@ -9,14 +9,14 @@ bool chain_initialized = false;
 MarkovChain __chain;
 
 extern "C" {
-    void initialize_chain(char* p) { 
+    void initialize_chain(char* p) {
         std::string path(p);
         std::ifstream in(path);
 
         std::string a, b, c;
         int d;
 
-        while(in >> a >> b >> c >> d) __chain[a][b][c] = d;
+        while (in >> a >> b >> c >> d) __chain[a][b][c] = d;
 
         chain_initialized = true;
     }
@@ -25,21 +25,21 @@ extern "C" {
         char* end = context + len - 1;
 
         std::string ult, pen;
-        while(*end && *(end--) != ' ') ult = *(end+1) + ult;
-        while(*end && *(end--) != ' ') pen = *(end+1) + pen;
+        while (*end && *(end--) != ' ') ult = *(end+1) + ult;
+        while (*end && *(end--) != ' ') pen = *(end+1) + pen;
 
         std::map<std::string, int> kv = __chain[pen][ult];
 
         int sum = 0;
-        for(auto it = kv.begin(); it != kv.end(); ++it) sum += it->second;
+        for (auto it = kv.begin(); it != kv.end(); ++it) sum += it->second;
 
-        if(sum == 0) return 0;
+        if (sum == 0) return 0;
 
         int chosen = rand() % sum;
         auto it = kv.begin();
-        while(it != kv.end() && chosen > 0) chosen -= (it++)->second;
+        while (it != kv.end() && chosen > 0) chosen -= (it++)->second;
 
-        if(it == kv.end() || it->first.length() >= maximum) return -1;
+        if (it == kv.end() || it->first.length() >= maximum) return -1;
 
         strcpy(out, it->first.c_str());
         return it->first.length();
